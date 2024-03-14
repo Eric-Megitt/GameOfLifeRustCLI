deb82b591214d551e6cb9fd7d6a06e60adce651b
Eric-Megitt <ericmegitt@hotmail.com>
2024-03-14 22:31:19 +0100

    Finished CLI app

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,34 @@ fn main() {
 
     //init
     board[5][5] = 1;
+    board[5][6] = 1;
+    board[5][7] = 1;
 
     //simulation
     loop {
-        //tick();
-        clearscreen::clear();
         print_board(board);
+        tick(&mut board);
         sleep(Duration::from_secs(1));
+        clearscreen::clear();
+    }
+}
+
+fn tick(board: &mut[[i32; SIZE]; SIZE]) {
+    let mut adjecent_live_neighbours: [[i32; SIZE]; SIZE] = [[0; SIZE]; SIZE];
+    for i in 0..SIZE {
+        for j in 0..SIZE {
+            adjecent_live_neighbours[i][j] = count_live_neighbours(i as i32, j as i32, &board) as i32;
+        }
+    }
+
+    for i in 0..SIZE {
+        for j in 0..SIZE {
+            match adjecent_live_neighbours[i][j] {
+                2 => {},
+                3 => board[i][j] = 1,
+                _ => board[i][j] = 0,
+            }
+        }
     }
 }
 
@@ -26,15 +47,15 @@ fn print_board(board: [[i32; SIZE]; SIZE]) {
     }
 }
 
-fn count_live_neighbours(position_x: i32, position_y: i32, board: [[i32; SIZE]; SIZE]) -> i32 {
+fn count_live_neighbours(position_x: i32, position_y: i32, board: &[[i32; SIZE]; SIZE]) -> i32 {
     let mut count = 0;
-    for i in -1..1 {
-        for j in -1..1 {
+    for i in -1..=1 {
+        for j in -1..=1 {
             if i == 0_i32 && j == 0_i32 {
                 continue;
             } 
                 
-            if position_x + i >= 0 && position_y + j >= 0 && position_x + i < (SIZE as i32) && position_y + j < (SIZE as i32) {
+            if position_x + i >= 0 && position_x + i < (SIZE as i32) - 1 && position_y + j >= 0 && position_y + j < (SIZE as i32) - 1 {
                 if board[(position_x + i) as usize][(position_y + j) as usize] == 1 {
                     count += 1;
                 }
